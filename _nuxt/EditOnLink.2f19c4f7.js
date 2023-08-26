28a6f70745adfcf14414bd72da7d22e8026f2dea
Gibran Dimasagung <gibrandev@gmail.com>
2023-08-26 15:29:57 +0700

    Change design

---
+++ _nuxt/EditOnLink.2f19c4f7.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./EditOnLink.vue.cfbb39d4.js";import"./entry.3f8c01f4.js";export{m as default};
---
+++ _nuxt/NuxtImg.632cdad3.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./NuxtImg.vue.1cea3161.js";import"./entry.3f8c01f4.js";export{m as default};
